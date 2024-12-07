65ed67c0fbf3b3ba4c2f6edeac6e68d393f59f46
rnw10va <rnw10va@gmail.com>
2024-12-07 13:26:09 -0700

    dev_beta_v0.2 branch created. Bug fixed: Feature 1 does not need a page refresh to appear. Feature Added: Feature 2 is a link to isthereanydeal.com on game info pages. Feature Added: Feature 3 is a link to howlongtobeat.com on gamem info pages.

---
+++ user-list-page.js
@@ -0,0 +1,27 @@
+var headerButton = document.getElementsByClassName("col-auto ml-auto mt-auto mb-2 pr-0");
+
+if (headerButton[0]) {
+	var randomizerButton = document.createElement("Button");
+	randomizerButton.id = "uniqueExtensionButtonId";
+	randomizerButton.innerHTML = "Random Pick";
+	randomizerButton.className = "btn btn-general";
+	randomizerButton.style.marginTop = "7%";
+	randomizerButton.onclick = onbuttonclicked;
+	headerButton[0].appendChild(randomizerButton);
+}
+
+function onbuttonclicked() {
+	const gameNames = document.getElementsByClassName("game-text-centered");
+	let gameNameForClipboard = ""
+	for (const gameName of gameNames){
+		gameNameForClipboard += gameName.innerText + "\n"
+	}
+	navigator.clipboard.writeText(gameNameForClipboard)
+	.then(() => {
+		console.log("Text copied to clipboard");
+	})
+	.catch(err => {
+		console.error("Failed to write to clipboard:", err);
+	});
+	const wheelWindow = window.open("https://wheelofnames.com/");
+}
